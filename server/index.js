f117870dedb0deb63e9651cc8cd9df606de23508
mounika2121 <chmounika2121@gmail.com>
2024-02-18 08:44:52 +0530

    chat room created

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ io.on("connection", (socket)=>{
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
     })
 
+    socket.on("send_message", (data)=>{
+        socket.to(data.room).emit("receive_msg", data);
+    })
+
     socket.on("disconnect" , ()=>{
         console.log("User Disconnected", socket.id);
     })
